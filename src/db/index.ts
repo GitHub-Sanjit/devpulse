2997f7fe8ad8202468d838565d5b072b6ffb879f
sanjit <sanjit.est14@gmail.com>
2026-05-20 23:30:11 +0600

    users table created in database

--- src/db/index.ts
+++ src/db/index.ts
@@ -10,17 +10,17 @@ export const initDB = async () => {
     await pool.query(`
         CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
+        name VARCHAR(255) NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
         password TEXT NOT NULL,
-        role VARCHAR(10) DEFAULT 'contributor',
+        role VARCHAR(255) DEFAULT 'contributor',
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
         )
       `)
     let l = config.connectionString.length;
     console.log(
-      `Database Connected Successfully, Total connectionString character ${l}`,
+      `Database Connected Successfully, Total connectionString character ${l} `,
     );
   } catch (error) {
     console.log({ error: error });
