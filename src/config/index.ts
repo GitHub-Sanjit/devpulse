7952940bed35c408643b72b6678743ae672006b6
sanjit <sanjit.est14@gmail.com>
2026-05-20 19:19:33 +0600

    folder structure and config file

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import path from "path";
+import { env } from "process";
+
+dotenv.config({
+  path: path.join(process.cwd(), ".env"),
+});
+
+const config = {
+  connectionString: env.CONNECTIONSTRING as string,
+  port: env.PORT,
+  jwt_secret: env.JWT_SECRET as string,
+  refresh_secret: env.JWT_REFRESH_SECRET as string,
+};
+
+export default config;
