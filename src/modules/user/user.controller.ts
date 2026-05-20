57f843c87da25549e363ea3ce6daeede431f2445
sanjit <sanjit.est14@gmail.com>
2026-05-20 23:12:49 +0600

    user creation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,30 @@
 import type { Request, Response } from "express";
+import { userServices } from "./user.service";
+import { sendResponse } from "../../utils/sendResponse";
 
-const createUser = (req: Request, res: Response) => {
-    const {name, email, passwrod, role} = req.body
+const createUser = async (req: Request, res: Response) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const result = await userServices.createUserIntoDB({
+      name,
+      email,
+      password,
+      role,
+    });
+    sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: "User Created Successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    sendResponse(res, {
+      statusCode: 500,
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
 };
 
 export const userController = {
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,22 @@
-import type { IUser } from "./user.interface";
+import bcrypt from "bcrypt";
 
-const createUserIntoDB = (payload: IUser) => {
-    
+import type { IUser } from "./user.interface";
+import { pool } from "../../db";
+
+const createUserIntoDB = async (payload: IUser) => {
+  const { name, email, password, role } = payload;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const result = await pool.query(
+    `
+      INSERT INTO users(name, email, password, role) VALUES($1, $2, $3, COALESCE($4,'contributor'))
+    `,
+    [name, email, hashedPassword, role],
+  );
+
+  delete result.rows[0].password;
+  return result;
 };
 
 export const userServices = {
