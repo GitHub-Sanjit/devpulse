c26b3dfdf657e81d063a2bc46080592b81a4be88
sanjit <sanjit.est14@gmail.com>
2026-05-20 20:48:22 +0600

    Common Response Pattern

---
+++ src/utils/sendResponse.ts
@@ -0,0 +1,10 @@
+import type { Response } from "express";
+import type { TResponse } from "../types";
+
+export const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    message: data.message,
+    data: data.data,
+  });
+};
