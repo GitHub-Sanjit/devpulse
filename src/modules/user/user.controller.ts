1d43e8be6741b1557e197f35e46403c3255374a6
sanjit <sanjit.est14@gmail.com>
2026-05-20 22:47:08 +0600

    user controller setup

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,7 @@
+import type { Request, Response } from "express";
+
+const createUser = (req: Request, res: Response) => {};
+
+export const userController = {
+  createUser,
+};
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { userController } from "./user.controller";
 
-const router = Router()
+const router = Router();
+
+router.post("/", userController.createUser);
 
 export const userRouter = router;
